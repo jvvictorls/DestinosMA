e0d286a98a55da88037a12df65150f56599f5296
João Victor <jvvictorls@hotmail.com>
2024-08-05 14:35:27 -0300

    Fix: Database para deploy

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -3,4 +3,15 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(config);
 
-export default sequelize;
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  } finally {
+    await sequelize.close();
+  }
+}
+
+testConnection();
