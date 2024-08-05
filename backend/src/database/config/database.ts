2d74a7a83afaa7673d5276a8b82bb421bd44c8ac
João Victor <jvvictorls@hotmail.com>
2024-08-05 17:12:51 -0300

    Fix: Database connection

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,8 +3,8 @@ import { Options } from "sequelize";
 const config: Options = {
   username: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  host: process.env.MYSQLHOST,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST || 'localhost',
   port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
 } 
