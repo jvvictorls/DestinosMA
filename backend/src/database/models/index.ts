c78d3409f7291a9e7c5d5a32018f563d3abd2eca
João Victor <jvvictorls@hotmail.com>
2024-08-05 14:40:49 -0300

    Fix: Removi o close da connection

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -9,8 +9,6 @@ async function testConnection() {
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
-  } finally {
-    await sequelize.close();
   }
 }
 
