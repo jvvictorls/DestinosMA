48385f4ba4f6da0520850dac5ed38b92bb4f9da2
João Victor <jvvictorls@hotmail.com>
2024-08-05 23:55:52 -0300

    Feat: última tentativa real oficial

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+const PORT = process.env.PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 3000,
+    port: Number(PORT),
   },
 });
