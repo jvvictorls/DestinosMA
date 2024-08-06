fe3ea1cec360232fe87535bd4a0496f4529b8692
João Victor <jvvictorls@hotmail.com>
2024-08-05 22:41:59 -0300

    Fix: frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
-    port: 3000,
+    host: '0.0.0.0',
   },
 });
